cc62aa1b1b98faed09a86f87febeef7d8422420f
Jatin Rishi <rishisbn2014@gmail.com>
2021-12-01 15:32:35 +0530

    Day1 : Part A and Part B Completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,65 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var previousValue = Int.MAX_VALUE
+        var depthIncreases = 0
+
+        for(item in input)
+        {
+            if(item.toInt() > previousValue)
+            {
+                depthIncreases+=1
+            }
+            previousValue = item.toInt()
+        }
+
+        return  depthIncreases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        val slidingSum: ArrayList<Int> = arrayListOf()
+
+        var ptr1 = 0
+        var ptr2 = 0
+        var sum =0
+
+        for(i in 0..2)
+        {
+            if(i<input.size)
+            {
+                sum+= input[i].toInt()
+                ptr2++
+            }
+            else
+            {
+                break
+            }
+        }
+        slidingSum.add(sum)
+
+        while(ptr2<input.size)
+        {
+            sum-=input[ptr1].toInt()
+            ptr1++
+
+            sum+=input[ptr2].toInt()
+            ptr2++
+            slidingSum.add(sum)
+        }
+        var previousValue = Int.MAX_VALUE
+        var depthIncreases = 0
+
+        for(item in slidingSum)
+        {
+            if(item > previousValue)
+            {
+                depthIncreases+=1
+            }
+            previousValue = item
+        }
+        return  depthIncreases
+
+    }
 
     val input = readInput("Day01")
     println(part1(input))
